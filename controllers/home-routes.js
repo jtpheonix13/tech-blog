2d2645e93faf9957f63cba81627d2213f59cb098
jtpheonix13 <jgtihunt@comcast.net>
2023-06-25 16:44:20 -0500

    finished out the home-routes.js file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,9 +4,23 @@ const { Post, Comment, User } = require('../models/');
 // get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    // TODO: 1. Find all Posts and associated Users
-    // TODO: 2. Serialize data (use .get() method, or use raw: true, nest: true in query options)
-    // TODO: 3. Render the 'all-posts' template with the posts data
+    //Find all Posts and associated Users
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+
+    //Serialize data (use .get() method, or use raw: true, nest: true in query options)
+    const post = postData.map((post) => {
+      post.get({ plain: true });
+    });
+
+    //Render the 'all-posts' template with the posts data
+    res.render('all-posts', { posts, loggedIn: req.session.loggedIn});
 
   } catch (err) {
     res.status(500).json(err);
@@ -16,9 +30,30 @@ router.get('/', async (req, res) => {
 // get single post
 router.get('/post/:id', async (req, res) => {
   try {
-    // TODO: 1. Find a single Post by primary key and include associated User and Comments (Comment will also need to include a User)
-    // TODO: 2. Serialize data (use .get() method, or use raw: true, nest: true in query options)
-    // TODO: 3. Render the 'single-post' template with the post data
+    //Find a single Post by primary key and include associated User and Comments (Comment will also need to include a User)
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        }
+      ]
+    });
+
+    //Serialize data (use .get() method, or use raw: true, nest: true in query options)
+    const post = postData.get({ plain: true });
+
+    //Render the 'single-post' template with the post data
+    res.render('single-post', {post, logged_in: req.session.loggedIn});
 
   } catch (err) {
     res.status(500).json(err);
