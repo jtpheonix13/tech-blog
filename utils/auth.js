d37a35f7d1692f53c5913f57827cec783214e514
jtpheonix13 <jgtihunt@comcast.net>
2023-06-25 16:25:38 -0500

    finished out the auth.js file

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,10 @@
 const withAuth = (req, res, next) => {
-
+    // if the user isn't logged in redirect them to the login route
+    if (!res.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
 };
 
 module.exports = withAuth;
